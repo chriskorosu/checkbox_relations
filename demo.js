069c371f463b71de605ea1a317c1057fb2c012ab
Chris <chriskorosu@gmail.com>
2023-06-30 21:28:25 -0400

    removed obsolete code and data structure

--- demo.js
+++ demo.js
@@ -1,15 +1,6 @@
-// const WORDLUXE_MODE = 0
-// const WORDLE_MODE = 1
-// const WORDLE_HARD_MODE = 2
-// const PREDICTION_BARS = 3
-// const COPY_PERFECT_MATCH = 4
-// const LOCK_COPIED_MATCH = 5
-// const USE_REVEALED_HINT = 6
-// const ALLOW_PLURALS = 7
-
 import { addCheckboxRelations } from './main.js'
 
-const checkboxRelations2 = {
+const checkboxRelations = {
   parents: ['parent1', 'parent2', 'parent3', 'custom'],
   children: {
     'child1': [true, false, false],
@@ -22,63 +13,4 @@ const checkboxRelations2 = {
   last_fallback_state: [true, true, true, true, true]
 }
 
-const checkboxRelations = {
-  parent1: {
-    children: {
-      child1: true,
-      child2: true,
-      child3: true,
-      child4: false,
-      child5: false,
-      parent2: false,
-      parent3: false
-    },
-    parents: []
-  },
-  parent2: {
-    children: {
-      child1: false,
-      child2: false,
-      child3: false,
-      child4: false,
-      child5: false,
-      parent1: false,
-      parent3: false
-    },
-    parents: []
-  },
-  parent3: {
-    children: {
-      child1: false,
-      child2: false,
-      child3: false,
-      child4: true,
-      child5: false,
-      parent1: false,
-      parent2: false
-    },
-    parents: []
-  },
-  child1: {
-    children: {},
-    parents: ['parent1', 'parent2', 'parent3']
-  },
-  child2: {
-    children: {},
-    parents: ['parent1', 'parent2', 'parent3']
-  },
-  child3: {
-    children: {},
-    parents: ['parent1', 'parent2', 'parent3']
-  },
-  child4: {
-    children: {},
-    parents: ['parent1', 'parent2', 'parent3']
-  },
-  child5: {
-    children: {},
-    parents: ['parent1', 'parent2', 'parent3']
-  }
-}
-
-addCheckboxRelations(checkboxRelations2)
+addCheckboxRelations(checkboxRelations)
